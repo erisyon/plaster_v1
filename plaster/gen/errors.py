c9b6ccfbbf3ba1a48f960938fea23699916d4d39
Zack <zack@erisyon.com>
2021-12-07 15:08:01 -0600

    cleanup

--- plaster/gen/errors.py
+++
@@ -1,8 +0,0 @@
-"""
-To avoid circular references, this class needs to be in a separate file
-"""
-
-
-class ValidationError(Exception):
-    # Raise this for any sort of CLI command error
-    pass
